1adf8c59658c773f466032d0bb9f28d5939cfdc3
piyush-ppradhan <pradhanpiyush687@gmail.com>
2024-09-28 20:08:05 -0400

    Latest

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,9 +1,5 @@
-require("tokyonight").setup({
-	style = "night",
-	transparent = false,
-	styles = {
-		keywords = { bold = true },
-	},
-})
+vim.cmd.colorscheme("retrobox")
 
-vim.cmd.colorscheme("tokyonight")
+vim.api.nvim_set_hl(0, "@variable.python", { fg = "#d5c4a1" })
+vim.api.nvim_set_hl(0, "@lsp.type.variable.c", { fg = "#d5c4a1" })
+vim.api.nvim_set_hl(0, "@lsp.type.variable.cpp", { fg = "#d5c4a1" })
