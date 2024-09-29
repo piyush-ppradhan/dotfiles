0e4847a348ad334964bd2c266b4337629519bfcb
piyush-ppradhan <pradhanpiyush687@gmail.com>
2024-09-28 20:11:52 -0400

    Improved colorscheme for highlight groups

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,5 +1,7 @@
 vim.cmd.colorscheme("retrobox")
 
 vim.api.nvim_set_hl(0, "@variable.python", { fg = "#d5c4a1" })
+vim.api.nvim_set_hl(0, "@variable.matlab", { fg = "#d5c4a1" })
+vim.api.nvim_set_hl(0, "@variable.javascript", { fg = "#d5c4a1" })
 vim.api.nvim_set_hl(0, "@lsp.type.variable.c", { fg = "#d5c4a1" })
 vim.api.nvim_set_hl(0, "@lsp.type.variable.cpp", { fg = "#d5c4a1" })
