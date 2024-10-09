fdff5fdcb1b6817fe49d4731c54b3db88a233caf
piyush-ppradhan <pradhanpiyush687@gmail.com>
2024-10-09 08:35:43 -0400

    LSP cleanup

--- .config/nvim/lua/piyush/lazy.lua
+++ .config/nvim/lua/piyush/lazy.lua
@@ -24,6 +24,28 @@ require("lazy").setup({
 			{ "hrsh7th/cmp-path" },
 			{ "hrsh7th/cmp-buffer" },
 		},
+		config = function()
+			-- Here is where you configure the autocompletion settings.
+			local lsp_zero = require("lsp-zero")
+			lsp_zero.extend_cmp()
+
+			local cmp = require("cmp")
+			local cmp_select = { behavior = cmp.SelectBehavior.Select }
+			local cmp_mappings = lsp_zero.defaults.cmp_mappings({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			})
+			--local cmp_action = lsp_zero.cmp_action()
+
+			cmp_mappings["<Tab>"] = nil
+			cmp_mappings["<S-Tab>"] = nil
+
+			cmp.setup({
+				mapping = cmp.mappings,
+			})
+		end,
 	},
 
 	-- LSP
@@ -49,6 +71,69 @@ require("lazy").setup({
 			{ "hrsh7th/cmp-nvim-lsp" },
 			{ "williamboman/mason-lspconfig.nvim" },
 		},
+		config = function()
+			local lsp_zero = require("lsp-zero")
+			lsp_zero.extend_lspconfig()
+
+			lsp_zero.on_attach(function(client, bufnr)
+				local opts = { buffer = bufnr, remap = false }
+
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, opts)
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, opts)
+				vim.keymap.set("n", "<leader>vws", function()
+					vim.lsp.buf.workspace_symbol()
+				end, opts)
+				vim.keymap.set("n", "<leader>vd", function()
+					vim.diagnostic.open_float()
+				end, opts)
+				vim.keymap.set("n", "[d", function()
+					vim.diagnostic.goto_next()
+				end, opts)
+				vim.keymap.set("n", "]d", function()
+					vim.diagnostic.goto_prev()
+				end, opts)
+				vim.keymap.set("n", "<leader>vca", function()
+					vim.lsp.buf.code_action()
+				end, opts)
+				vim.keymap.set("n", "<leader>vrr", function()
+					vim.lsp.buf.references()
+				end, opts)
+				vim.keymap.set("n", "<leader>vrn", function()
+					vim.lsp.buf.rename()
+				end, opts)
+				vim.keymap.set("i", "<C-h>", function()
+					vim.lsp.buf.signature_help()
+				end, opts)
+			end)
+
+			lsp_zero.set_preferences({
+				suggest_lsp_servers = false,
+				sign_icons = {
+					error = "E",
+					warn = "W",
+					hint = "H",
+					info = "I",
+				},
+			})
+
+			require("mason-lspconfig").setup({
+				ensure_installed = { "clangd", "pyright", "julials" },
+				handlers = {
+					lsp_zero.default_setup,
+					lua_ls = function()
+						local lua_opts = lsp_zero.nvim_lua_ls()
+						require("lspconfig").lua_ls.setup(lua_opts)
+					end,
+					julials = function()
+						require("lspconfig").julials.setup({ julia_env_path = "~/.julia/environments/v1.10" })
+					end,
+				},
+			})
+		end,
 	},
 
 	-- Colorscheme
