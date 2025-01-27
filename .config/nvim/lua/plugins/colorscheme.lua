e015369232c705530951279e57f886967a171a13
piyush-ppradhan <pradhanpiyush687@gmail.com>
2025-01-26 23:31:57 -0500

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -13,11 +13,25 @@ return {
 				},
 			})
 
-			vim.cmd.colorscheme("rose-pine")
-			vim.api.nvim_set_hl(0, "Normal", { bg = "#050505" })
-			vim.api.nvim_set_hl(0, "NormalNC", { bg = "#050505" })
-			vim.api.nvim_set_hl(0, "SignColumn", { bg = "#050505" })
-			vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#050505" })
+			-- vim.cmd.colorscheme("rose-pine")
+			-- vim.api.nvim_set_hl(0, "Normal", { bg = "#050505" })
+			-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "#050505" })
+			-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "#050505" })
+			-- vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#050505" })
+		end,
+	},
+	{
+		"oxfist/night-owl.nvim",
+		config = function()
+			require("night-owl").setup({
+				bold = true,
+				italics = false,
+				underline = true,
+				undercurl = true,
+				transparent_background = true,
+			})
+			vim.cmd.colorscheme("night-owl")
+			vim.api.nvim_set_hl(0, "SignColumn", { bg = "None" })
 		end,
 	},
 }
