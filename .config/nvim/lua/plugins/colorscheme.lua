b8e402b235fb01fbeadc38e90cfe7805f5f075b5
piyush-ppradhan <pradhanpiyush687@gmail.com>
2025-02-04 20:51:51 -0500

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,12 +3,9 @@ return {
 		"rose-pine/neovim",
 		config = function()
 			require("rose-pine").setup({
-				main = "main",
-				dark_variant = "main",
 				styles = {
 					bold = false,
 					italic = false,
-					transparency = true,
 				},
 			})
 			vim.cmd.colorscheme("rose-pine")
