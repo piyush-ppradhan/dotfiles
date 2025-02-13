fe5bd2abd74ba4ded6c4d06f479bbf3dcef91e23
piyush-ppradhan <pradhanpiyush687@gmail.com>
2025-02-13 06:21:27 -0500

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -18,9 +18,11 @@ return {
 		"eemed/sitruuna.vim",
 		config = function()
 			vim.cmd.colorscheme("sitruuna")
+			vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#272727", fg = "#526262" })
 			vim.api.nvim_set_hl(0, "StatusLine", { fg = "#526262", bg = "None" })
 			vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
 			vim.api.nvim_set_hl(0, "NormalNC", { bg = "None" })
+			vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#526262", bg = "None" })
 			vim.api.nvim_set_hl(0, "LineNr", { fg = "#526262", bg = "None" })
 			vim.api.nvim_set_hl(0, "SignColumn", { bg = "None" })
 		end,
