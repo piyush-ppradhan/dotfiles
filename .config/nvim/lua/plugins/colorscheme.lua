077cf96a38014367127225206377d23b4da00df4
piyush-ppradhan <pradhanpiyush687@gmail.com>
2025-02-15 23:05:14 -0500

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,14 @@
 return {
 	{
-		"RRethy/base16-nvim",
+		"sainnhe/gruvbox-material",
 		lazy = false,
 		config = function()
-			vim.cmd.colorscheme("base16-black-metal-gorgoroth")
-			vim.api.nvim_set_hl(0, "@variable.builtin.python", { fg = "#5f8787", bg = "None", italic = false })
-			vim.api.nvim_set_hl(0, "@function.builtin.python", { fg = "#888888", bg = "None", italic = false })
+			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_better_performance = 1
+			vim.g.gruvbox_material_foreground = "material"
+			vim.cmd.colorscheme("gruvbox-material")
+			vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
+			vim.api.nvim_set_hl(0, "NormalNC", { bg = "None" })
 		end,
 	},
 }
