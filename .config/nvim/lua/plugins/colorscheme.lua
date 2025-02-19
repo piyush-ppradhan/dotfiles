0a9aac10a77c0d5418b5951fa6fa9144f1e21665
Piyush Pradhan <pradhan@Piyushs-Laptop-2.local>
2025-02-18 21:10:31 -0500

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,27 +1,22 @@
 return {
 	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
+		"catppuccin/nvim",
 		config = function()
-			-- vim.g.gruvbox_material_background = "hard"
-			-- vim.g.gruvbox_material_better_performance = 1
-			-- vim.g.gruvbox_material_foreground = "material"
-			-- vim.cmd.colorscheme("gruvbox-material")
-			-- vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
-			-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "None" })
-		end,
-	},
-	{
-		"nickkadutskyi/jb.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("jb").setup({ transparent = true })
-			vim.cmd.colorscheme("jb")
-			vim.api.nvim_set_hl(0, "StatusLine", { fg = "#4b5059", bg = "None" })
-			vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "#4b5059", bg = "None" })
-			vim.api.nvim_set_hl(0, "MsgArea", { fg = "#4b5059", bg = "None" })
-			vim.api.nvim_set_hl(0, "@string.documentation.python", { fg = "#5f826b", italic = false })
+			require("catppuccin").setup({
+				flavour = "mocha",
+				background = {
+					light = "latte",
+					dark = "mocha",
+				},
+				transparent_background = true,
+				no_italic = true,
+				no_bold = false,
+				styles = {
+					comments = {},
+					conditionals = {},
+				},
+			})
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 }
