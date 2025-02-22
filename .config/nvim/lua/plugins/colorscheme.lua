26c91507283f8774f3299a7eb688788e3e75843b
piyush-ppradhan <pradhanpiyush687@gmail.com>
2025-02-22 02:22:06 -0500

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,8 +3,8 @@ return {
 		"rose-pine/neovim",
 		config = function()
 			require("rose-pine").setup({
-				variant = "moon",
-				dark_variant = "moon",
+				variant = "main",
+				dark_variant = "main",
 				dim_inactive_windows = false,
 				extend_background_behind_borders = true,
 
