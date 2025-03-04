5f6e66cce346eb77fc341a2f761df8df71a99439
Piyush Pradhan <pradhan@Piyushs-Laptop-2.local>
2025-03-04 11:46:20 -0500

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
 		config = function()
 			vim.g.gruvbox_transparent_bg = 1
 			vim.g.gruvbox_contrast_light = "hard"
-			vim.opt.background = "light"
+			vim.opt.background = "dark"
 			vim.cmd.colorscheme("gruvbox")
 			vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
 			vim.api.nvim_set_hl(0, "NormalNC", { bg = "None" })
