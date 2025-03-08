72b5d814452a0b06b4c291e34947c1a9477e7e64
Piyush Pradhan <pradhan@Piyushs-Laptop-2.local>
2025-03-07 19:57:14 -0500

    gruvbox

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -7,12 +7,15 @@ return {
 			vim.g.gruvbox_contrast_light = "hard"
 			vim.g.gruvbox_contrast_dark = "hard"
 			vim.cmd.colorscheme("gruvbox")
-			vim.api.nvim_set_hl(0, "Normal", { fg = "#f1f0e9", bg = "None" })
-			vim.api.nvim_set_hl(0, "NormalNC", { fg = "#f1f0e9", bg = "None" })
+			vim.api.nvim_set_hl(0, "Normal", { fg = "#cec3a3", bg = "None" })
+			vim.api.nvim_set_hl(0, "NormalNC", { fg = "#cec3a3", bg = "None" })
 			-- vim.api.nvim_set_hl(0, "StatusLine", { bg = "#e0dfdb" })
 			-- vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#e0dfdb" })
-			-- vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#e0dfdb" })
-			-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "None" })
+			vim.api.nvim_set_hl(0, "SignColumn", { bg = "None" })
+			vim.api.nvim_set_hl(0, "@variable", { fg = "#cec3a3" })
+			vim.api.nvim_set_hl(0, "@variable.python", { fg = "#cec3a3" })
+			vim.api.nvim_set_hl(0, "@variable.member.python", { fg = "#cec3a3" })
+			vim.api.nvim_set_hl(0, "Delimiter", { fg = "#cec3a3" })
 		end,
 	},
 }
