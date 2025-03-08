cb1f28fdfd11288a6dfc39fd40d15dc04489163a
Piyush Pradhan <pradhanpiyush687@gmail.com>
2025-03-07 19:35:43 -0500

    latest

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,16 +2,17 @@ return {
 	{
 		"morhetz/gruvbox",
 		config = function()
-			vim.opt.background = "light"
+			vim.opt.background = "dark"
 			vim.g.gruvbox_transparent_bg = 1
 			vim.g.gruvbox_contrast_light = "hard"
+			vim.g.gruvbox_contrast_dark = "hard"
 			vim.cmd.colorscheme("gruvbox")
-			vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
-			vim.api.nvim_set_hl(0, "NormalNC", { bg = "None" })
-			vim.api.nvim_set_hl(0, "StatusLine", { bg = "#e0dfdb" })
-			vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#e0dfdb" })
-			vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#e0dfdb" })
-			vim.api.nvim_set_hl(0, "SignColumn", { bg = "None" })
+			vim.api.nvim_set_hl(0, "Normal", { fg = "#f1f0e9", bg = "None" })
+			vim.api.nvim_set_hl(0, "NormalNC", { fg = "#f1f0e9", bg = "None" })
+			-- vim.api.nvim_set_hl(0, "StatusLine", { bg = "#e0dfdb" })
+			-- vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#e0dfdb" })
+			-- vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#e0dfdb" })
+			-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "None" })
 		end,
 	},
 }
