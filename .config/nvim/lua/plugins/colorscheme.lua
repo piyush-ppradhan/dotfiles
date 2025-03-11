fc1f49b2bb3a14b4c9290e8cefaf3024c17e9421
Piyush Pradhan <pradhanpiyush687@gmail.com>
2025-03-11 05:12:40 -0400

    gruvbox

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,24 @@
 return {
 	{
-		"polirritmico/monokai-nightasty.nvim",
+		"ellisonleao/gruvbox.nvim",
 		config = function()
-			vim.cmd.colorscheme("monokai-nightasty")
+			require("gruvbox").setup({
+				bold = true,
+				italic = {
+					strings = true,
+					emphasis = false,
+					comments = true,
+					operators = false,
+					folds = false,
+				},
+				contrast = "hard",
+			})
+			vim.cmd.colorscheme("gruvbox")
 			vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
 			vim.api.nvim_set_hl(0, "NormalNC", { bg = "None" })
 			vim.api.nvim_set_hl(0, "SignColumn", { bg = "None" })
 			vim.api.nvim_set_hl(0, "StatusLine", { bg = "None", bold = true })
+			vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "None" })
 		end
 	}
 }
