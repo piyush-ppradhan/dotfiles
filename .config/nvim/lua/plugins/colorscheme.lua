c184ad039a51d2dfe5e5cf961acf893fe92aca56
Piyush Pradhan <pradhanpiyush687@gmail.com>
2025-07-26 04:15:30 -0400

    latest

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,35 @@
 return {
 	{
-		"ellisonleao/gruvbox.nvim",
+		"echasnovski/mini.base16",
+		version = false,
 		config = function()
-			require("gruvbox").setup({
-				terminal_colors = true,
-				italic = {
-					strings = true,
-					emphasis = true,
-					comments = true,
-					operators = false,
-					folds = true,
-				},
-				contrast = "hard",
-				transparent_mode = true,
-			})
-			vim.cmd.colorscheme("gruvbox")
-			vim.api.nvim_set_hl(0, "@type.python", { fg = "#f7bc2d", bold = true })
+			vim.cmd.colorscheme("minicyan")
+			vim.api.nvim_set_hl(0, "Normal", { bg = "#041010" })
+			vim.api.nvim_set_hl(0, "NormalNC", { bg = "#041010" })
+			vim.api.nvim_set_hl(0, "LineNr", { fg = "#5f8788", bg = "None" })
+			vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { fg = "#ef4436", bg = "None", underline = true })
+			vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { fg = "#ffbf04", bg = "None", underline = true })
+			vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { fg = "#3e87fa", bg = "None", underline = true })
+			vim.api.nvim_set_hl(0, "DiagnosticSignError", { fg = "#ef4436", bg = "None" })
+			vim.api.nvim_set_hl(0, "DiagnosticSignWarn", { fg = "#ffbf04", bg = "None" })
+			vim.api.nvim_set_hl(0, "DiagnosticSignHint", { fg = "#3e87fa", bg = "None" })
+			vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#ef4436", bg = "#373737" })
+			vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { fg = "#ffbf04", bg = "#373737" })
+			vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { fg = "#3e87fa", bg = "#373737" })
+			vim.api.nvim_set_hl(0, "SignColumn", { bg = "None" })
+			vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#556868", bg = "None" })
+			vim.api.nvim_set_hl(0, "Keyword", { fg = "#ffb8df", bg = "None", bold = true })
+			vim.api.nvim_set_hl(0, "StatusLine", { fg = "#babbb9", bg = "None", bold = true })
+			vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "#babbb9", bg = "None" })
+			vim.api.nvim_set_hl(0, "MsgArea", { fg = "#babbb9", bg = "None" })
+			vim.api.nvim_set_hl(0, "Type", { fg = "#80e1b9", bg = "None" })
+			vim.api.nvim_set_hl(0, "String", { fg = "#7DB589", bg = "None" })
+			vim.api.nvim_set_hl(0, "Delimiter", { fg = "#c5c7bf", bg = "None" })
+			vim.api.nvim_set_hl(0, "Number", { fg = "#e4c07b", bg = "None" })
+			vim.api.nvim_set_hl(0, "Float", { fg = "#e4c07b", bg = "None" })
+			vim.api.nvim_set_hl(0, "Constant", { fg = "#e4c07b", bg = "None" })
+			vim.api.nvim_set_hl(0, "@keyword.return", { fg = "#f9b8df", bg = "None", bold = true })
+			vim.api.nvim_set_hl(0, "@comment.todo", { fg = "#259569", bg = "None", bold = true })
 		end,
 	},
 }
