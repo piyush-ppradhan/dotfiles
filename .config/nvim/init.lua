143b81e4bbd8ac4c3b269a9256cec225e84952a1
Piyush Pradhan <pradhanpiyush687@gmail.com>
2025-09-01 22:53:27 -0400

    latest

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,25 @@
 vim.loader.enable()
 vim.g.mapleader = " "
+vim.opt.guicursor = ""
+vim.opt.termguicolors = true
+-- vim.opt.nu = true
+-- vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.inccommand = "split"
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.updatetime = 50
+vim.opt.autoread = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.fillchars = { eob = " " }
+vim.opt.winborder = "rounded"
+vim.api.nvim_set_option_value("clipboard", "unnamedplus", {})
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -33,25 +53,29 @@ require("lazy").setup({ import = "plugins" }, {
 	},
 })
 
--- local signs = {
--- 	Error = "",
--- 	Warn = "",
--- 	Hint = "",
--- 	Info = " ",
--- }
--- for type, icon in pairs(signs) do
--- 	local hl = "DiagnosticSign" .. type
--- 	vim.diagnostic.config({ text = icon, texthl = hl, numhl = hl }, hl)
--- end
+-- vim.diagnostic.config({
+-- 	-- enable signs and customize their text per severity
+-- 	signs = {
+-- 		text = {
+-- 			[vim.diagnostic.severity.ERROR] = "",
+-- 			[vim.diagnostic.severity.WARN] = "",
+-- 			[vim.diagnostic.severity.HINT] = "",
+-- 			[vim.diagnostic.severity.INFO] = "",
+-- 		},
+-- 	},
+-- 	virtual_text = true,
+-- 	underline = true,
+-- 	severity_sort = true,
+-- })
 
 vim.diagnostic.config({
 	-- enable signs and customize their text per severity
 	signs = {
 		text = {
-			[vim.diagnostic.severity.ERROR] = "",
-			[vim.diagnostic.severity.WARN] = "",
-			[vim.diagnostic.severity.HINT] = "",
-			[vim.diagnostic.severity.INFO] = "",
+			[vim.diagnostic.severity.ERROR] = "E",
+			[vim.diagnostic.severity.WARN] = "W",
+			[vim.diagnostic.severity.HINT] = "H",
+			[vim.diagnostic.severity.INFO] = "I",
 		},
 	},
 	virtual_text = true,
