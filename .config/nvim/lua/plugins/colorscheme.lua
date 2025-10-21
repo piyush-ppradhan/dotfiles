ef5fd356fe2a37fc426477fc3fc5aade61d77457
Piyush Pradhan <pradhanpiyush687@gmail.com>
2025-10-21 03:07:34 -0400

    Latest

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,25 +1,22 @@
 vim.pack.add({
-	-- { src = "https://github.com/ellisonleao/gruvbox.nvim" },
-	{ src = "https://github.com/blazkowolf/gruber-darker.nvim" },
+	{ src = "https://github.com/ellisonleao/gruvbox.nvim" },
 })
 
--- require("gruvbox").setup({
--- 	bold = true,
--- 	contrast = "hard",
--- 	transparent_mode = true,
--- })
--- vim.cmd.colorscheme("gruvbox")
-
--- vim.cmd.colorscheme("default_vim")
-
-require("gruber-darker").setup({ bold = true })
-
-vim.cmd.colorscheme("gruber-darker")
-vim.api.nvim_set_hl(0, "Normal", { bg = "None", fg = "#ffffff" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "None", fg = "#ffffff" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None", fg = "#ffffff" })
+require("gruvbox").setup({
+	terminal_colors = true,
+	bold = true,
+	contrast = "hard",
+	transparent_mode = true,
+})
+vim.cmd.colorscheme("gruvbox")
 
 vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { bg = "#303030", fg = "#f43841", bold = true })
 vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { bg = "#303030", fg = "#ffdd33", bold = true })
 vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { bg = "#303030", fg = "#9e95c7", bold = true })
 vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { bg = "#303030", fg = "#9e95c7", bold = true })
+vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { bg = "#303030", fg = "#9e95c7", bold = true })
+
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { bg = "None", fg = "#f43841", bold = true, undercurl = true })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { bg = "None", fg = "#ffdd33", bold = true, undercurl = true })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { bg = "None", fg = "#9e95c7", bold = true, undercurl = true })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { bg = "None", fg = "#9e95c7", bold = true, undercurl = true })
