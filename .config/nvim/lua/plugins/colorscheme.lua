c39747d397818c76926990a2b6f183ecdb5dbf06
Piyush Pradhan <pradhanpiyush687@gmail.com>
2025-11-04 09:04:10 -0500

    Latest

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,7 @@
 vim.pack.add({
 	{ src = "https://github.com/rose-pine/neovim" },
+	{ src = "https://github.com/ellisonleao/gruvbox.nvim" },
+	{ src = "https://github.com/wkflanders/anysphere-dark.nvim" },
 })
 
 require("rose-pine").setup({
@@ -11,7 +13,26 @@ require("rose-pine").setup({
 		transparency = false,
 	},
 })
-vim.cmd.colorscheme("rose-pine")
+-- vim.cmd.colorscheme("rose-pine")
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "None", fg = "#e0def4" })
+-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "None", fg = "#e0def4" })
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "None", fg = "#e0def4" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "None", fg = "#e0def4" })
+require("gruvbox").setup({
+	terminal_colors = true, -- add neovim terminal colors
+	undercurl = true,
+	underline = true,
+	bold = true,
+	italic = {
+		strings = false,
+		emphasis = false,
+		comments = true,
+		operators = false,
+		folds = true,
+	},
+	contrast = "hard",
+	transparent_mode = true,
+})
+vim.cmd.colorscheme("gruvbox")
+
+vim.cmd.colorscheme("anysphere")
+vim.api.nvim_set_hl(0, "Normal", { bg = "None", fg = "#d6d6dd" })
