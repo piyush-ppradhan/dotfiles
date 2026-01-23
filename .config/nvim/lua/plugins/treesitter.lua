356901352653b8f6ed0e6f8a3820331c35bdb265
piyush-ppradhan <pradhanpiyush687@gmail.com>
2026-01-22 23:05:35 -0500

    latest

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -31,3 +31,14 @@ require("nvim-treesitter.config").setup({
 		additional_vim_regex_highlighting = false,
 	},
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	callback = function(args)
+		local max_filesize = 100 * 1024 -- 100 KB
+		local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(args.buf))
+		if ok and stats and stats.size > max_filesize then
+			return
+		end
+		pcall(vim.treesitter.start, args.buf)
+	end,
+})
